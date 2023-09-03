0fa06b6389eaf23dcf9bce1eb8f23ade1d3f0521
Emori Nagumo <emoriiin979@gmail.com>
2023-09-03 18:41:59 +0900

    画面表示まで完了

---
+++ resources/js/Pages/Wiki/Search.vue
@@ -0,0 +1,15 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+    <GuestLayout>
+        <Head title="Search" />
+
+        <div class="mb-4 text-sm text-gray-600">
+            Search
+        </div>
+    </GuestLayout>
+</template>
