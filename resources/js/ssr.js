3a9186100142cd223e4081cf974b31b0949b4fb6
Emori Nagumo <emoriiin979@gmail.com>
2023-09-06 23:36:47 +0900

    ssr.jsにもVuetify導入

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -5,6 +5,18 @@ import createServer from '@inertiajs/vue3/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+import 'vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
 const appName = 'Laravel';
 
 createServer((page) =>
@@ -15,6 +27,7 @@ createServer((page) =>
         resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
         setup({ App, props, plugin }) {
             return createSSRApp({ render: () => h(App, props) })
+                .use(vuetify)
                 .use(plugin)
                 .use(ZiggyVue, {
                     ...page.props.ziggy,
